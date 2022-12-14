bd336b33b1a483dcebab428cd71dea3729fdb7fa
Volodymyr Kott <vladimirkott19@gmail.com>
2022-12-14 18:07:13 +0200

    +

--- index.js
+++ index.js
@@ -1,43 +1,55 @@
-const yargs = require("yargs");
-const {addNote, printNotes, remove} = require('./notes.controller')
+const express = require('express')
+const chalk = require('chalk')
+const path = require('path')
+const {addNote, getNotes, editNote, removeNote} = require('./notes.controller')
 
-yargs.command({
-    command: 'add',
-    discribe: 'add new note to list',
-    builder:{
-        title: {
-            type: 'string',
-            describe: 'Note title',
-            demandOption: true
-        }
-    },
-    handler({title}){
-        addNote(title)
-    }
+const port = 3000
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', 'pages')
+
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'public')))
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.get('/', async (req, res) => {
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: false
+  })
 })
 
-yargs.command({
-    command: 'list',
-    discribe: 'print all notes',
-    async handler(){
-        const notes = await printNotes()
-        console.log('List note: ', notes)
-    }
+app.post('/', async (req, res) => {
+  await addNote(req.body.title)
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: true
+  })
 })
 
-yargs.command({
-    command: 'remove',
-    discribe: 'remove element by ID',
-    builder: {
-        id: {
-            type: 'string',
-            discribe: 'id of exist note',
-            demandOption: true
-        }
-    },
-    async handler({id}){
-        remove(id)
-    }
+app.put('/:id', async (req, res) => {
+    await editNote(req.params.id)
+    res.render('index', {
+      title: 'Express App',
+      notes: await getNotes(),
+      created: false
+    })
+  })
+
+app.delete('/:id', async (req, res) => {
+  await removeNote(req.params.id)
+  res.render('index', {
+    title: 'Express App',
+    notes: await getNotes(),
+    created: false
+  })
 })
 
-yargs.parse()
+app.listen(port, () => {
+  console.log(chalk.green(`Server has been started on port ${port}...`))
+})
--- notes.controller.js
+++ notes.controller.js
@@ -2,44 +2,56 @@ const fs = require('fs/promises')
 const path = require('path')
 const chalk = require('chalk')
 
-const notePath = path.join(__dirname, 'db.json')
+const notesPath = path.join(__dirname, 'db.json')
 
+async function addNote(title) {
+  const notes = await getNotes()
+  const note = {
+    title,
+    id: Date.now().toString()
+  }
 
-async function addNote(title){
-    //const notes = require('./db.json')
-    const notes = await getNotes()
-    const note={
-        title,
-        id: Date.now().toString() 
-    }
-    notes.push(note)
-    await fs.writeFile(notePath, JSON.stringify(notes))
-    console.log(chalk.bgGreen('note was added'))
+  notes.push(note)
+
+  await saveNotes(notes)
+  console.log(chalk.bgGreen('Note was added!'))
 }
 
-async function printNotes(){
-    const notes = await getNotes()
-    console.log(chalk.bgBlue('Yor notes:'))
-    notes.forEach(n => {
-        console.log(chalk.bgBlue(n.id + ' ' + n.title))
-    });
+async function getNotes() {
+  const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
+  return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
 }
 
-async function remove (id){
-    const notes = await getNotes()
-    const index = notes.findIndex(n => {
-        return n.id === id
-    })
-    notes.splice(index, 1)
-    await fs.writeFile(notePath, JSON.stringify(notes))
-    console.log(chalk.bgYellow('You note was deleted'))
+async function saveNotes(notes) {
+  await fs.writeFile(notesPath, JSON.stringify(notes))
 }
 
-async function getNotes(){
-    const notes = await fs.readFile(notePath, {encoding: 'utf-8'})
-    return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes):[]
+async function printNotes() {
+  const notes = await getNotes()
+  console.log(chalk.bgBlue('Here is the list of notes:'))
+  notes.forEach(note => {
+    console.log(chalk.bgWhite(note.id), chalk.blue(note.title))
+  })
+}
+
+async function editNote(data){
+  const notes = await getNotes()
+  const parseData = JSON.parse(data)
+  const note = notes.findIndex(n => n.id == parseData.id);
+  notes[note].title = parseData.string;
+  await saveNotes(notes)
+  console.log(chalk.yellow(`Note with id="${parseData.id}" has been edit.`))
+}
+
+async function removeNote(id) {
+  const notes = await getNotes()
+
+  const filtered = notes.filter(note => note.id !== id)
+
+  await saveNotes(filtered)
+  console.log(chalk.red(`Note with id="${id}" has been removed.`))
 }
 
 module.exports = {
-    addNote, printNotes, remove
+  addNote, getNotes, editNote, removeNote
 }
